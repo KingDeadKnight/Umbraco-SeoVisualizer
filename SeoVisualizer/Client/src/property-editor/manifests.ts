45141eb91d26e67cd336a1841ba73a9e62785108
KingDeadKnight <15180531+KingDeadKnight@users.noreply.github.com>
2024-10-05 12:19:14 +0200

    Add data type settings

--- SeoVisualizer/Client/src/property-editor/manifests.ts
+++ SeoVisualizer/Client/src/property-editor/manifests.ts
@@ -7,18 +7,40 @@ const schema : ManifestPropertyEditorSchema = {
     meta : {
         defaultPropertyEditorUiAlias : 'EnkelMedia.SeoVisualizer.PropertyEditorUi',
         settings : {
-            properties : [
+            properties: [
                 {
-                    alias : 'min',
-                    label : 'Minimum number of items',
-                    description : '',
-                    propertyEditorUiAlias : 'Umb.PropertyEditorUi.Integer'
+                    label : "Recommended number of characters for Title warning",
+                    description : "Will show warning when the title contains more chars than defined. (Default is 60).",
+                    alias : "maxCharsTitle",
+                    propertyEditorUiAlias : "Umb.PropertyEditorUi.Integer"
                 },
                 {
-                    alias : 'max',
-                    label : 'Maximum number of items',
-                    description : '',
-                    propertyEditorUiAlias : 'Umb.PropertyEditorUi.Integer'
+                    label : "Recommended number of characters for Description warning",
+                    description : "Will show warning when the description contains more chars than defined. (Default is 160).",
+                    alias : "maxCharsDescription",
+                    propertyEditorUiAlias : "Umb.PropertyEditorUi.Integer",
+                },
+                {
+                    label : "Default Title Suffix",
+                    description : "Will append a suffix to the Title preview (not included in the saved data)",
+                    alias : "titleSuffix",
+                    propertyEditorUiAlias : "Umb.PropertyEditorUi.TextBox"
+                },
+                {
+                    label : "Show noindex-option",
+                    description : "Use this to show a noindex-toggle in the editor",
+                    alias : "useNoIndex",
+                    propertyEditorUiAlias : "Umb.PropertyEditorUi.Toggle"
+                }
+            ],
+            defaultData : [
+                {
+                    alias : "maxCharsTitle",
+                    value : 60
+                },
+                {
+                    alias : "maxCharsDescription",
+                    value : 160
                 }
             ]
         }
